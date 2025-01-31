69da8af8a6008a7992f259316afe8a4722f5308d
henryflia <superflia2059@gmail.com>
2025-01-31 17:17:21 -0400

    W4

--- utilities/account-validation.js
+++ utilities/account-validation.js
@@ -1,5 +1,7 @@
 const utilities = require("../utilities/")
 const express = require("express")
+const accountModel = require("../models/account-model")
+
 const { body, validationResult } = require("express-validator")
 const validate = {}
 
@@ -26,13 +28,49 @@ validate.registationRules = () => {
         .withMessage("Please provide a last name."), // on error this message is sent.
   
       // valid email is required and cannot already exist in the DB
+      // valid email is required and cannot already exist in the database
       body("account_email")
-      .trim()
-      .escape()
-      .notEmpty()
-      .isEmail()
-      .normalizeEmail() // refer to validator.js docs
-      .withMessage("A valid email is required."),
+        .trim()
+        .isEmail()
+        .normalizeEmail() // refer to validator.js docs
+        .withMessage("A valid email is required.")
+        .custom(async (account_email) => {
+          const emailExists = await accountModel.checkExistingEmail(account_email)
+          if (emailExists){
+            throw new Error("Email exists. Please log in or use different email")
+          }
+      }),
+  
+      // password is required and must be strong password
+      body("account_password")
+        .trim()
+        .notEmpty()
+        .isStrongPassword({
+          minLength: 12,
+          minLowercase: 1,
+          minUppercase: 1,
+          minNumbers: 1,
+          minSymbols: 1,
+        })
+        .withMessage("Password does not meet requirements."),
+    ]
+  }
+
+  validate.LoginRules = () => {
+    return [
+      // valid email is required and cannot already exist in the DB
+      // valid email is required and cannot already exist in the database
+      body("account_email")
+        .trim()
+        .isEmail()
+        .normalizeEmail() // refer to validator.js docs
+        .withMessage("A valid email is required.")
+        .custom(async (account_email) => {
+          const emailExists = await accountModel.checkExistingEmail(account_email)
+          if (emailExists){
+            throw new Error("Email exists. Please log in or use different email")
+          }
+      }),
   
       // password is required and must be strong password
       body("account_password")
@@ -72,4 +110,24 @@ validate.checkRegData = async (req, res, next) => {
     next()
   }
   
-  module.exports = validate
+/* ******************************
+ * Check data and return errors or continue to registration
+ * ***************************** */
+validate.checkLoginData = async (req, res, next) => {
+  const { account_email } = req.body;
+  let errors = [];
+  errors = validationResult(req);
+  if (!errors.isEmpty()) {
+      let nav = await utilities.getNav();
+      res.render("account/login", {
+          errors,
+          title: "Login",
+          nav,
+          account_email,
+      });
+      return;
+  }
+  next();
+};
+
+module.exports = validate
