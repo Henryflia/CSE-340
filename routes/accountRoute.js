bd30ba2612c8fc8bc7a19ef2b52d0e734e4a833c
henryflia <superflia2059@gmail.com>
2025-01-31 15:23:13 -0400

    Push

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,13 +3,21 @@ const express = require("express")
 const router = new express.Router() 
 const utilities = require("../utilities/index.js")
 const accountController = require("../controllers/accountController")
+const regValidate = require('../utilities/account-validation')
 
 
 // Route to build inventory by classification view
 router.get("/login", utilities.handleErrors(accountController.buildLogin))
-router.get("/register", utilities.handleErrors(accountController.buildRegister))
 
 // Route to Register
-router.post('/register', utilities.handleErrors(accountController.registerAccount))
+router.get("/register", utilities.handleErrors(accountController.buildRegister))
+// Process the registration data
+router.post(
+    "/register",
+    regValidate.registationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount)
+  )
+
 
 module.exports = router;
